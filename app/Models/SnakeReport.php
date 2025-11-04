2a7337646e8b5a77d323e172653f91945b836c49
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-04 10:29:15 +0530

    Update SnakeReport.php

--- app/Models/SnakeReport.php
+++ app/Models/SnakeReport.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SnakeReport extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable=[
+        'user_id',
+        'snake_type',
+        'location',
+        'description',
+        'status',
+    ];
 }
