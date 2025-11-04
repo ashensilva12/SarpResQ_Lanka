ffd2fd152c537cf87f91286cecfbc3868fa5ffff
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-04 10:56:43 +0530

    Update SnakeReportController.php

--- app/Http/Controllers/SnakeReportController.php
+++ app/Http/Controllers/SnakeReportController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SnakeReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SnakeReportController extends Controller
 {
@@ -15,12 +17,13 @@ class SnakeReportController extends Controller
             'snake_type'=>'nullable|string',
             'description'=>'nullable|string',
         ]);
-
         SnakeReport::create([
-            'user_id'=>auth()->id(),
+            'user_id'=>Auth::id(),
             'snake_type'=>$request->snake_type,
             'location'=>$request->location,
             'description'=>$request->description,
         ]);
+
+        return redirect()->back()->with('success','Snake report submitted successfully.');
     }
 }
