ed8be214240dcfa826d05c982ae0a6fd2f54da52
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-05 21:03:50 +0530

    Create home.blade.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.nav')
+
+@section('title', 'Home')
+
+@section('content')
+    <h2>Welcome to SarpResQ Lanka</h2>
+    <p>Your one-stop solution for reporting and tracking snake sightings.</p>
+@endsection
+
